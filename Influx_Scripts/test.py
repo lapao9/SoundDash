3cc017d26901b80fc9533ac3f0ff86539197111b
laa <laa@ISEL-LAA.isel.priv>
2026-06-09 13:47:31 +0100

    smth rm

--- Influx_Scripts/test.py
+++ Influx_Scripts/test.py
@@ -1,9 +1,9 @@
 from influxdb_client import InfluxDBClient
-client = InfluxDBClient(url="http://localhost:8086", token="meu-token-supersecreto", org="myorg")
+client = InfluxDBClient(url="http://10.64.137.6:8086/", token="LvrSeU4NaBeQN7c4S4LsJCmlflUIQDgwRJqm383tdMoQaoDkM6pHAB022sAURdkvsSG_SWGXp8FVKVciviD3iA==", org="ISEL")
 q = '''
 from(bucket: "SoundDashHosp")
   |> range(start: -1y)
-  |> filter(fn: (r) => r["_measurement"] == "Sensor100")
+  |> filter(fn: (r) => r["_measurement"] == "sensor2")
   |> count()
 '''
 print(client.query_api().query(q))
